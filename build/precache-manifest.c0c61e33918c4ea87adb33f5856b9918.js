9372b372c303c12ee7a3bb63cac68ade9c4639a2
Nurul Hidayat <nurul.hidayat@bukitmakmur.com>
2020-12-24 07:41:18 +0700

    init code

---
+++ build/precache-manifest.c0c61e33918c4ea87adb33f5856b9918.js
@@ -0,0 +1,162 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e88c76a62472196b6eb24b9b617e40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f58cef597802e653e5cd",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "502590859179c91c6869",
+    "url": "/static/css/main.d91c8596.chunk.css"
+  },
+  {
+    "revision": "f58cef597802e653e5cd",
+    "url": "/static/js/2.5f14ebfe.chunk.js"
+  },
+  {
+    "revision": "c714d000bf161c4e2c797d977b1dbdd1",
+    "url": "/static/js/2.5f14ebfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "502590859179c91c6869",
+    "url": "/static/js/main.fbd123cd.chunk.js"
+  },
+  {
+    "revision": "2bdd9edbcb0d05446ac6",
+    "url": "/static/js/runtime-main.892f9b02.js"
+  },
+  {
+    "revision": "aeb6e814d8479323758fb6b380cf787a",
+    "url": "/static/media/analytic-icon.aeb6e814.svg"
+  },
+  {
+    "revision": "52aed0ef3826b48ff0155d6a27cf9f4e",
+    "url": "/static/media/arrow-custom.52aed0ef.svg"
+  },
+  {
+    "revision": "8dec29149f43099b04ce0f4cff4148f1",
+    "url": "/static/media/arrow-white.8dec2914.svg"
+  },
+  {
+    "revision": "11ed7b5ac627d97b6e8d7afb9196d68e",
+    "url": "/static/media/avatar-dummy.11ed7b5a.svg"
+  },
+  {
+    "revision": "f96dad7f2540ec6db5f8b30b39b4238c",
+    "url": "/static/media/calendar-icon.f96dad7f.svg"
+  },
+  {
+    "revision": "1863d946c9bd2a4ba76625146e14af9c",
+    "url": "/static/media/categories-icon.1863d946.svg"
+  },
+  {
+    "revision": "e20df962d99d33a5a953fa8e2ec0c5c8",
+    "url": "/static/media/dashboard-icon.e20df962.svg"
+  },
+  {
+    "revision": "5d5c89756b45dc01410a3d90b7ebfb0b",
+    "url": "/static/media/email-icon.5d5c8975.svg"
+  },
+  {
+    "revision": "446d4692c4e95ac79c5f17f0659dd30b",
+    "url": "/static/media/eye-icon-white.446d4692.svg"
+  },
+  {
+    "revision": "6fc0e4cbbafecc15d538db66b10e2f6f",
+    "url": "/static/media/eye-icon.6fc0e4cb.svg"
+  },
+  {
+    "revision": "268880bde770917c7f8f473c9f3a86b3",
+    "url": "/static/media/fb-icon-blue.268880bd.svg"
+  },
+  {
+    "revision": "04172a2ca49c3c3144c2375be5989e61",
+    "url": "/static/media/fb-icon.04172a2c.svg"
+  },
+  {
+    "revision": "3c29bc0ddd702fb4e0b3155c445c4a92",
+    "url": "/static/media/google-icon-colorful.3c29bc0d.svg"
+  },
+  {
+    "revision": "f7e8a7f526ad78e6d014e5327415849e",
+    "url": "/static/media/hamburger.f7e8a7f5.svg"
+  },
+  {
+    "revision": "78c373af49b124b737e18faaf789e7f0",
+    "url": "/static/media/handphone.78c373af.png"
+  },
+  {
+    "revision": "af742826b560f93680d48ce7034ae5f5",
+    "url": "/static/media/icon-play.af742826.svg"
+  },
+  {
+    "revision": "138eb0b09ce1c77420bf3f3581fd3499",
+    "url": "/static/media/icon-search.138eb0b0.svg"
+  },
+  {
+    "revision": "16e022ca396f8f5718ff6021bbf05cc7",
+    "url": "/static/media/ig-icon.16e022ca.svg"
+  },
+  {
+    "revision": "90b5a014334b428795d68aecc52cfca8",
+    "url": "/static/media/login-icon.90b5a014.svg"
+  },
+  {
+    "revision": "3c6927f40a1380823c98fc394a5d9784",
+    "url": "/static/media/logout-icon.3c6927f4.svg"
+  },
+  {
+    "revision": "56d6b119532879495455f9f67be17cb5",
+    "url": "/static/media/merchantlist-icon.56d6b119.svg"
+  },
+  {
+    "revision": "c95f03f1fcc7f6785950dd1dc1607785",
+    "url": "/static/media/password-icon.c95f03f1.svg"
+  },
+  {
+    "revision": "1acd62e902947a1024b467784cb70d54",
+    "url": "/static/media/pito-small.1acd62e9.svg"
+  },
+  {
+    "revision": "fe7aeb6dba534be4254f048053d0ec95",
+    "url": "/static/media/pito.fe7aeb6d.svg"
+  },
+  {
+    "revision": "8ec08e8ef61233fa5fc86669aa9f4941",
+    "url": "/static/media/share-icon-mobile.8ec08e8e.svg"
+  },
+  {
+    "revision": "662fd5706e737d85c2c3a32460569fc0",
+    "url": "/static/media/share-icon.662fd570.svg"
+  },
+  {
+    "revision": "35483ffb455ee104644e8d903576df33",
+    "url": "/static/media/subscriber-icon.35483ffb.svg"
+  },
+  {
+    "revision": "90a52487f131faf1244cabc293fd72f1",
+    "url": "/static/media/thumbnail-one.90a52487.jpg"
+  },
+  {
+    "revision": "dd062c8cca6160400f41c7cd4e4b192c",
+    "url": "/static/media/thumbs-like-icon.dd062c8c.svg"
+  },
+  {
+    "revision": "4972d2554f8e60e934e0f6ed104ee114",
+    "url": "/static/media/ticketsup-icon.4972d255.svg"
+  },
+  {
+    "revision": "0c7336faaf325e3e3e56f58909046390",
+    "url": "/static/media/tiktok-icon.0c7336fa.svg"
+  },
+  {
+    "revision": "9b7bec67e5d42a192a0d2178fa1d80a7",
+    "url": "/static/media/user-avatar.9b7bec67.jpg"
+  },
+  {
+    "revision": "ba1dcb618e10f6b17ac1ce57c6a82a45",
+    "url": "/static/media/userlist-icon.ba1dcb61.svg"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+//import PrivateRoutes
+import PrivateRoute from 'Routes/PrivateRoute'
+// import PrivateMerchantRoute from 'Routes/PrivateMerchantRoute'
+
+//import pages admin
+import Login from 'pages/login'
+import ForgotPassword from 'pages/forgotPassword'
+import Dashboard from 'pages/dashboard'
+import LivestreamList from 'pages/livestream-list'
+import MerchantList from 'pages/merchant-list'
+import MerchantDetail from 'pages/merchant-detail'
+import UserListing from 'pages/user-list'
+import Tickets from 'pages/tickets'
+import Categories from 'pages/categories'
+import Analytic from 'pages/analytic'
+import userDetail from 'pages/user-detail'
+import NotFound404 from 'pages/404'
+
+//style from tailwindcss
+import 'assets/css/main.css';
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route path="/forgot-password" component={ForgotPassword} />
+        <PrivateRoute exact path="/dashboard" component={Dashboard} />
+        <PrivateRoute path="/livestream-list" component={LivestreamList} />
+        <Route path="/merchant-list"
+          render={({ match: { url } }) => (
+            <>
+              <PrivateRoute path={`${url}/`} component={MerchantList} exact />
+              <PrivateRoute path={`${url}/:id`} component={MerchantDetail} />
+            </>
+          )} />
+        <Route path="/user-list"
+          render={({ match: { url } }) => (
+            <>
+              <PrivateRoute path={`${url}/`} component={UserListing} exact />
+              <PrivateRoute path={`${url}/:id`} component={userDetail} />
+            </>
+          )} />
+        <PrivateRoute path="/tickets" component={Tickets} />
+        <PrivateRoute path="/categories" component={Categories} />
+        <PrivateRoute path="/analytic" component={Analytic} />
+       
+        <Route path="*" component={NotFound404} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
