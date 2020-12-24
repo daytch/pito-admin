e7f139cf83681daa446f2a7bfcfc1195fb492612
Nurul Hidayat <nurul.hidayat@bukitmakmur.com>
2020-12-24 07:45:38 +0700

    fixing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 //import PrivateRoutes
 import PrivateRoute from 'Routes/PrivateRoute'
-// import PrivateMerchantRoute from 'Routes/PrivateMerchantRoute'
 
 //import pages admin
 import Login from 'pages/login'
